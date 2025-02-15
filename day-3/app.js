f8592fd7234385e8cabfa4552211b89b6f82bd44
Alessandro Magionami <alessandro.magionami@gmail.com>
2025-02-15 18:14:41 +0100

    day 3 review

--- day-3/app.js
+++ day-3/app.js
@@ -1,27 +1,37 @@
-import path from "node:path";
-import AutoLoad from "@fastify/autoload";
+import { fastify } from 'fastify'
+import { loggerConfig } from './logger-config.js'
 
-// Pass --options via CLI arguments in command to enable these options.
-export const options = {};
+const app = fastify({
+  disableRequestLogging: true,
+  logger: loggerConfig
+})
 
-export default async function (fastify, opts) {
-  // Place here your custom code!
+// Using a preHandler to also have access to the parsed body
+// @see https://github.com/fastify/fastify/issues/1686#issuecomment-498563121
+app.addHook('preHandler', async function onRequestLogHook(req) {
+  req.log.info({ req }, 'incoming request 🔮')
+})
 
-  // Do not touch the following lines
+app.get('/formatter', { handler: formatError })
+app.get('/root', { handler: helloHandler, config: { logBody: true } })
+app.post('/sensitive', { handler: sensitiveHandler, config: { logBody: true } })
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(import.meta.dirname, "plugins"),
-    options: Object.assign({}, opts),
-  });
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(import.meta.dirname, "routes"),
-    routeParams: true,
-    options: Object.assign({}, opts),
-  });
+async function helloHandler(req, reply) {
+  return { hello: 'world' }
 }
+
+async function formatError(req, reply) {
+  const obj = { big: 'object' }
+  req.log.debug(`Incorrect: ${JSON.stringify(obj)}`)
+  req.log.debug('Correct: %o', obj)
+  return { hello: 'world' }
+}
+
+async function sensitiveHandler(req, reply) {
+  return { hello: 'world' }
+}
+
+await app.inject('/formatter')
+await app.inject('/root')
+await app.inject({ url: '/sensitive', method: 'POST', body: { name: 'non-sensitive', password: 'hide-me-please' } })
+
